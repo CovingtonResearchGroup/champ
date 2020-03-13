6a3a3200c4d015eeab0a5a4368bdcb8039f8d073
Matt Covington <speleophysics@gmail.com>
2020-03-13 17:55:10 +0100

    erode function

--- crossSection.py
+++ crossSection.py
@@ -1,22 +1,26 @@
 from numpy import sin, cos, pi, fabs, sign, roll, arctan2, diff, sum, hypot,\
                     logical_and, where, linspace, sqrt
+import numpy as np
 from scipy import interpolate
 from scipy.optimize import brentq
 import matplotlib.pyplot as plt
 
 
 g=9.8 #m/s^2
+rho_w = 998.2 #kg/m^3
 SMALL = 1e-6
+use_centroid_fraction =0.98#switch to  max vel at centroid if over this fraction of ymax
 
 class CrossSection:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, z0=0.01):#z0=roughness height
         self.n = len(x)
         self.x = x
         self.y = y
         self.ymin = min(y)
         self.ymax = max(y)
         self.create_pm()
+        self.z0=z0
 
     # Create arrays of x+1, y+1, x-1, x+1
     def create_pm(self):
@@ -56,6 +60,136 @@ class CrossSection:
         R = where(condR)[0][0]
         return L,R
 
+    def setFD(self,fd):
+        maxdepth = self.ymax - self.ymin
+        if fd>maxdepth:
+            fd=maxdepth
+        self.fd = fd
+        self.wetidx = self.y - self.ymin<=fd
+
+    def setMaxVelPoint(self,fd):
+        self.setFD(fd)
+        if fd>(self.ymax-self.ymin)*use_centroid_fraction:
+            #Treat as full pipe
+            mx, my = self.findCentroid()
+        else:
+            #open channel
+            L,R = self.findLR(fd)
+            mx = 0
+            my = self.y[R]
+        self.xmaxVel = mx
+        self.ymaxVel = my
+
+    def findCentroid(self):
+        m = self.xm*self.y-self.x*self.ym
+        A = self.calcA()
+        cx = (1/(6*A))*((self.x + self.xm)*m).sum()#A was self.sA. not sure if this matters
+        cy = (1/(6*A))*((self.y + self.ym)*m).sum()
+        return cx, cy
+
+    def calcR_l(self, wantidx=None):
+        if type(wantidx) != type(None):
+            self.r_l = np.hypot(self.x[wantidx]-self.xmaxVel, self.y[wantidx]-self.ymaxVel)
+        else:
+            self.r_l = np.hypot(self.x-self.xmaxVel, self.y-self.ymaxVel)
+        return self.r_l
+
+	# Find the value of U_max by weighted law of the wall
+    def calcUmax(self, Q, method="area"):
+        wetidx = self.wetidx#self.y-self.ymin <self.fd
+        r_l = self.calcR_l(wantidx=wetidx)
+        z0 = self.z0
+        Ax = self.xmaxVel
+        Ay = self.ymaxVel
+        Bx = roll(self.x[wetidx],1)
+        By = roll(self.y[wetidx],1)
+        Cx = self.x[wetidx]
+        Cy = self.y[wetidx]
+
+        a_top = Ax*(By-Cy) + Bx*(Cy-Ay) + Cx*(Ay-By)
+        a_i = fabs(a_top/2.)
+
+        if method=="line":
+            u_i = r_l/(r_l-z0) - 1/np.log(r_l/z0)
+
+        if method=="area":
+            u_top = r_l**2*np.log(r_l/z0) - 3./2*r_l**2 + 2.*r_l*z0 - z0**2/2.
+            u_bottom = z0**2 + r_l**2 - 2*z0*r_l
+            u_i = 1./np.log(r_l/z0)*u_top/u_bottom
+
+        self.umax = Q/(a_i*u_i).sum()
+
+    def calcT_b(self):
+        vgrad = self.calcVGrad2()
+        psi = self.calcPsi()
+        Awet = self.calcA(wantidx=self.wetidx)
+        self.T_b = psi*rho_w*Awet*self.vgrad2
+        return self.T_b
+
+    def calcVGrad2(self):
+        wetidx=self.wetidx
+        phi = np.arctan2(self.ymaxVel - self.y[wetidx],self.xmaxVel -self.x[wetidx])
+        alpha = np.arctan2(self.yp[wetidx] - self.ym[wetidx], self.xp[wetidx]-self.xm[wetidx])
+        alpha[0] = alpha[1]
+        alpha[-1] = alpha[-2]
+        self.vgrad2 = (self.umax/self.z0)*(1./np.log(self.r_l/self.z0))*np.fabs(np.sin(phi-alpha))**2
+        return self.vgrad2
+
+    def calcPsi(self):
+        wetidx = self.wetidx
+        l = np.hypot(self.x[wetidx] - self.xp[wetidx], self.y[wetidx] - self.yp[wetidx])
+        sum = ((self.vgrad2)*l).sum()
+        self.psi = g*self.eSlope/sum
+        return self.psi
+
+    def setEnergySlope(self,slope):
+        self.eSlope = slope
+
+    def erode(self, dr, resample=False, n=None):
+        if n==None:
+            n=self.n
+        wetidx=self.wetidx
+        theta = np.arctan2(self.xp-self.xm, self.yp-self.ym)
+        nx = self.x
+        ny = self.y
+        nx[wetidx] = self.x[wetidx] + dr*cos(theta[wetidx])
+        ny[wetidx] = self.y[wetidx] - dr*sin(theta[wetidx])
+
+
+
+        # Check if we drew inside or outside..
+        #  Think I fixed this MDC (3/13/2020)
+        #c = self.ccw(self.x[wetidx], self.y[wetidx], self.xm[wetidx], self.ym[wetidx], nx[wetidx], ny[wetidx])
+
+        #(nx[wetidx])[c] = (self.x[wetidx] - sign(self.x[wetidx])*dr*cos(theta))[c]
+        #(ny[wetidx])[c] = (self.y[wetidx] + sign(self.x[wetidx])*dr*sin(theta))[c]
+
+#        c = self.ccw(self.x, self.y, self.xm, self.ym, nx, ny)
+
+#        nx[c] = (self.x - sign(self.x)*dr*cos(theta))[c]
+#        ny[c] = (self.y + sign(self.x)*dr*sin(theta))[c]
+
+        #Resample points by fitting spline
+        if resample:
+            tck, u = interpolate.splprep([nx, ny], u=None, k=1, s=0.0)
+            un = linspace(u.min(), u.max(), n if n!=nx.size else nx.size)
+            nx, ny = interpolate.splev(un, tck, der=0)
+
+
+		# New coordinates
+        y_roll = ny.size - ny.argmax()
+        nx = roll(nx, y_roll)
+        ny = roll(ny, y_roll)
+        self.x = nx
+        self.y = ny
+        self.create_pm()
+
+    # Counter clockwise function to determine if we drew points in the correct
+    # direction
+    #def ccw(self, x, y, xm, ym, nx, ny):
+    #    return (x - xm) * (ny - ym) > (y - ym) * (nx - xm)
+
+
     def calcNormalFlow(self,depth, slope,f=0.1):
         wetidx = self.y - self.ymin < depth
         Pw = self.calcP(wantidx=wetidx)
@@ -92,6 +226,7 @@ class CrossSection:
                 return -1
         else:
             fd = brentq(self.normal_discharge_residual, SMALL, maxdepth, args=(slope,f,Q))
+        self.setFD(df)
         return fd
 
     def calcCritFlowDepth(self,Q):
