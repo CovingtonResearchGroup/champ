81f7e3158b201b196453bcb5555999fba50f80c0
Matt Covington <speleophysics@gmail.com>
2020-05-12 21:08:26 +0200

    added docstrings to single_XC and moved some old code.

--- single_XC_sim.py
+++ single_XC_sim.py
@@ -1,3 +1,5 @@
+"""Code to run simulations of single cross-sections."""
+
 import numpy as np
 from scipy.signal import savgol_filter
 
@@ -25,11 +27,48 @@ cm_m = 100.
 
 
 class single_XC:
-    def __init__(self, init_radius=1., init_shape='circle', Q_w=1., slope=0.001, impure=True,
+    def __init__(self, init_radius=1., Q_w=1., slope=0.001, impure=True,
         reduction_factor=0.01, dt_erode=1., pCO2_atm=5000*1e-6, Ca=0.5, f=0.1, T_cave = 10,
         xc_n = 1500):
+
+        """
+        Parameters
+        ----------
+        Q_w : float, optional
+            Discharge in the channel (m^3/s). Default is 0.1 m^3/s.
+        f : float, optional
+            Darcy-Weisbach friction factor (unitless), used in both water flow and air
+            flow calculations. Default is 0.1.
+        init_radii : float or ndarray, optional
+            Initial cross-section radii (meters). If a float then all cross-sections
+            will be assigned the same radius. If an array then each element
+            represents the radius of a single cross-section (length should be n-1
+            where n is the number of nodes). Default is 0.5 m.
+        xc_n : int, optional
+            Number of points that will define the cave passage shape within a cross-section.
+            Default is 1000.
+        slope : float
+            Prescribed channel slope. Default is 0.001.
+        pCO2_atm : float, optional
+            Dissolved pCO2 value in atm. Default is 5e-3 atm (5000 ppm).
+        Ca : float, optional
+            Dissolved Ca concentration expressed
+            as a fraction of saturation at a pCO2 of pCO2_atm. Default is 0.5.
+        T_cave : float, optional
+            Cave temperature (air and water) in degrees C. Default is 10 C.
+        reduction_factor : float, optional
+            Factor by which pure transport-limited dissolution rate will be multiplied.
+            This is used to reduce rates because the transport-limited equation creates
+            unrealistically high rates, even if field evidence suggests that rates are
+            to some extent transport-limited. Default value 0.01.
+        dt_erode : float, optional
+            Erosional time step in years. Default value is 1 year.
+        impure : boolean, optional
+            If the Palmer dissolution rate equation is used, this determines
+            whether to use the equation for impure or pure calcite. Default is True.
+        """
+
         self.init_radius = init_radius
-        self.init_shape = init_shape
         self.Q_w = Q_w
         self.slope = slope
         self.impure = impure
@@ -50,6 +89,7 @@ class single_XC:
         self.xc = CrossSection(x,y)
 
     def calc_flow_depth(self):
+        """Calculate flow depth."""
         old_fd = self.xc.fd
         self.xc.create_A_interp()
         self.xc.create_P_interp()
@@ -62,6 +102,7 @@ class single_XC:
             self.xc.setEnergySlope(self.slope)
 
     def erode_xc(self):
+        """Erode cross-section according to transport-limited equation."""
         #if palmer:
         #    this_Ca = self.Ca*self.Ca_eq_0
         #    sol = solutionFromCaPCO2(this_Ca, self.pCO2_atm, T_C=self.T_cave)
@@ -82,6 +123,7 @@ class single_XC:
         self.xc.erode(self.xc.dr)
 
     def erode_power_law(self, a=1., K=1e-5):
+        """Erode cross-section according to power law."""
         self.xc.erode_power_law(a=a, K=K)
 #        self.xc.setMaxVelPoint(self.xc.fd)
 #        self.xc.calcUmax(self.Q_w)
