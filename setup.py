00d67f6938c247ea440e663a41eaf8ccb2269cd6
Matt Covington <speleophysics@gmail.com>
2021-12-30 19:48:28 -0600

    changed fastCalcA module name in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from numpy import get_include
 
 ext_modules = [
     Extension(
-        "chansim/utils/fastCalcA",
+        "fastCalcA",
         ["chansim/utils/fastCalcA.pyx"],
         extra_compile_args=['-fopenmp'],
         extra_link_args=['-fopenmp'],
