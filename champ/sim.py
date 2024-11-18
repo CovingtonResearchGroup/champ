38c6d9dd7107465951190f7d9383cf7d1258f92f
Matt Covington <speleophysics@gmail.com>
2024-11-18 11:27:23 -0600

    only resample and trim XC for final, highest Q value

--- champ/sim.py
+++ champ/sim.py
@@ -427,7 +427,12 @@ class singleXC_multiQ(singleXC):
         for i, Q_w in enumerate(self.Q_arr):
             self.Q_w = Q_w
             self.calc_flow()
-            self.erode(dt_frac=self.pdf_Q_frac[i], no_erode=True)
+            if i == self.nQ - 1:
+                # For final Q we will resample and trim
+                self.erode(dt_frac=self.pdf_Q_frac[i])
+            else:
+                # No trimming or resampling for lower Q values
+                self.erode(dt_frac=self.pdf_Q_frac[i], resample=False, trim=False)
             dr_tot[self.xc.wetidx] += self.xc.dr
 
         # For multiQ sims this assumes largest discharge is last
