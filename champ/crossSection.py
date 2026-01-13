6533bf9087b9fa03ed899efdb9c77c41b84b0147
Charlotte Boehme <charlotteboehme1@gmail.com>
2026-01-13 16:00:10 -0600

    updare total xc updates every time instead of when switching to total

--- champ/crossSection.py
+++ champ/crossSection.py
@@ -507,7 +507,8 @@ class CrossSection:
             The new x and y points within the evolved cross-section.
 
         """
-        n = self.n + 2
+        self.n = self.n + 2
+        n = self.n
         # create new total xc arrays from old and wet portions
         self.x1 = x1 = self.x_total[
             np.logical_and(self.x_total < 0, self.y_total > ny.max())
